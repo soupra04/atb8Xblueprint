bbd5b6346310e588c354e9660fef34524ad11ec0
acer <acer@LAPTOP-G457OLQ6>
2024-09-19 09:00:44 +0530

    Task2

---
+++ atb8XDay1/src/day1/Task2Day18thSept.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Task2Day18thSept {
+
+	public static void main(String[] args) {
+		int a = 10; 
+		System.out.println(  --a + a++ + a--); 
+		//--a =9
+		//a++ =9
+		//a-- =10
+		
+		System.out.println(a);
+
+	}
+
+}
