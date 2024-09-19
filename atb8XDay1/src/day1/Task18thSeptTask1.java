504a55a402652982c8582c831df6146f4841eadd
acer <acer@LAPTOP-G457OLQ6>
2024-09-19 08:53:55 +0530

    Task1

---
+++ atb8XDay1/src/day1/Task18thSeptTask1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Task18thSeptTask1 {
+
+	public static void main(String[] args) {
+		int a = 10; 
+		System.out.println(  --a + a --  + a--);
+		// --a = 9
+		// a-- = 9
+		// a-- = 8
+		System.out.println(a);
+		
+	}
+
+}
